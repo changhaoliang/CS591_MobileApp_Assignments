ae3df67edaaea217c4b33d7048748702d21cccc0
zixiangwei <mir1102@163.com>
2020-02-08 15:50:52 -0500

    task_2_29

--- task_2_29/app/src/androidTest/java/com/example/calories/ExampleInstrumentedTest.java
+++ task_2_29/app/src/androidTest/java/com/example/calories/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.chessboard;
+package com.example.calories;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.chessboard", appContext.getPackageName());
+        assertEquals("com.example.calories", appContext.getPackageName());
     }
 }
