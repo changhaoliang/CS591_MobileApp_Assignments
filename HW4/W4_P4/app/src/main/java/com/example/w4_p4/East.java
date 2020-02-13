a4495d57cf381a370930ca4a0764f1efbe223608
changhao <changhao@bu.edu>
2020-02-13 15:35:45 -0500

    W4_P4 upload and initialize W4_P5

---
+++ HW4/W4_P4/app/src/main/java/com/example/w4_p4/East.java
@@ -0,0 +1,16 @@
+package com.example.w4_p4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.GestureDetector;
+
+public class East extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_east);
+    }
+}
