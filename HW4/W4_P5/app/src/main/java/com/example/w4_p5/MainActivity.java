b253614589d47e50e6b4fdd0471ea15a55f6d7d3
Wang Juntao <wangjt@zetyun.com>
2020-02-16 17:02:39 -0500

    123

--- HW4/W4_P5/app/src/main/java/com/example/w4_p5/MainActivity.java
+++ HW4/W4_P5/app/src/main/java/com/example/w4_p5/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.w4_p5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+        RelativeLayout myLayout = new RelativeLayout(this);
+        setContentView(myLayout);
+        WordInput wordInput = new WordInput(this, 5);
+        myLayout.addView(wordInput);
     }
 }
