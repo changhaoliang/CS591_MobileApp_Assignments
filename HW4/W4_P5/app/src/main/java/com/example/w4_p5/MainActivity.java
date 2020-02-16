2073af45be45cf42ee98102f8dbd22f4cdb9d9b5
zixiangwei <mir1102@163.com>
2020-02-16 15:56:48 -0500

    letter buttons for part5

--- HW4/W4_P5/app/src/main/java/com/example/w4_p5/MainActivity.java
+++ HW4/W4_P5/app/src/main/java/com/example/w4_p5/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.w4_p5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button[][] letterButtons;
+    private final int kbRows = 4, kbColumns = 7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void buildKeyboard(){
+        GridLayout keyBoard = new GridLayout(this);
+        keyBoard.setColumnCount(kbColumns);
+        keyBoard.setRowCount(kbRows);
+        letterButtons = new Button[kbRows][kbColumns];
+        ButtonHandler bh = new ButtonHandler();
+        for (int row=0; row<kbRows; row++){
+            for (int col=0; col<kbColumns; col++){
+                letterButtons[row][col] = new Button(this);
+                letterButtons[row][col].setText((char)(65+row*7+col)+"");
+                letterButtons[row][col].setOnClickListener(bh);
+            }
+        }
+
+    }
+    private boolean checkLetter(Button b){
+        char c = b.getText().charAt(0);
+        //if c is the answer return true, and set button invisible
+        return false;
+    }
+    private class ButtonHandler implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            for (int row=0; row<kbRows; row++){
+                for (int col=0; col<kbColumns; col++){
+                    if (v == letterButtons[row][col]){
+                        checkLetter(letterButtons[row][col]);
+                    }
+                }
+            }
+        }
     }
 }
