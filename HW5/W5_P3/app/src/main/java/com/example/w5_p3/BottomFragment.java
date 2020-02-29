1835b143d4559b32cb92f04d1d0b351c1242474c
zixiangwei <mir1102@163.com>
2020-02-29 15:03:36 -0500

    set p3 views

---
+++ HW5/W5_P3/app/src/main/java/com/example/w5_p3/BottomFragment.java
@@ -0,0 +1,28 @@
+package com.example.w5_p3;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class BottomFragment extends Fragment {
+
+    public BottomFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_bottom, container, false);   //before returning the inflated view.
+        return view;
+    }
+}
