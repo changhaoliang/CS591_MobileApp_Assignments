b46521d341f80f5d8388a1ad0d6a0c2c85866afa
宋彦杉 <songyanshan.2008@163.com>
2020-02-29 15:06:38 -0500

    update

--- HW5/W5_P3/app/src/main/java/com/example/w5_p3/PathCheck.java
+++ HW5/W5_P3/app/src/main/java/com/example/w5_p3/PathCheck.java
@@ -3,8 +3,8 @@ package com.example.w5_p3;
 
 public class PathCheck{
     public static boolean isAdjacent(int[] currentIndex, int[] nextIndex) {
-        double distance_square = Math.pow(currentIndex[0], nextIndex[0]) + Math.pow(currentIndex[1], nextIndex[1]);
-        return distance_square <= 2;
+        double distanceSquare = Math.pow(currentIndex[0], nextIndex[0]) + Math.pow(currentIndex[1], nextIndex[1]);
+        return distanceSquare <= 2;
     }
 }
 
