df098e3f94723f170f4ea0b1f09186fccbacb618
Juntao Wang <juntaow@bu.edu>
2020-03-28 17:25:42 -0400

    123

--- HW8/W8_P2/app/src/main/java/com/example/firebase/MainActivity.java
+++ HW8/W8_P2/app/src/main/java/com/example/firebase/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +22,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference myRef = firebaseDatabase.getReference("message");
-        myRef.setValue("Hello");
+        final ArrayList<String> arrayList = new ArrayList<>();
+        for(int i = 1; i <= 5; i++) {
+            arrayList.add(String.valueOf(i));
+        }
+        myRef.setValue(arrayList);
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ArrayList arrayList1 = dataSnapshot.getValue(ArrayList.class);
+                for (int i = 0; i < arrayList1.size(); i++) {
+                    System.out.println(arrayList1.get(i));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
