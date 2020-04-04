1c635abfe7688c26ff80a5fc305b5611d1135d74
changhao <changhao@bu.edu>
2020-04-04 16:53:57 -0400

    123

--- IngrediEAT/app/src/main/java/com/example/ingredieat/acitivity/LibraryActivity.java
+++ IngrediEAT/app/src/main/java/com/example/ingredieat/acitivity/LibraryActivity.java
@@ -77,12 +77,12 @@ public class LibraryActivity extends AppCompatActivity implements ItemFragement.
 
     @Override
     public void setMenu(boolean flag) {
-        if (flag == true) {
+        if (flag) {
             menuView.getMenu().removeItem(R.id.user);
             menuView.getMenu().removeItem(R.id.cook);
             menuView.getMenu().removeItem(R.id.favourite);
-            menuView.getMenu().removeItem(R.id.search);
-            menuView.getMenu().removeItem(R.id.add);
+            menuView.getMenu().removeItem(R.id.cart);
+            menuView.getMenu().removeItem(R.id.fridge);
 
             menuView.getMenu().add(1, R.string.delete, 1, R.string.delete);
             menuView.getMenu().add(1, R.string.cancel, 1, R.string.cancel);
@@ -98,14 +98,14 @@ public class LibraryActivity extends AppCompatActivity implements ItemFragement.
         menuView.getMenu().removeItem(R.string.cancel);
         menuView.getMenu().removeItem(R.string.delete);
 
-        menuView.getMenu().add(1, R.id.add, 1, R.string.add);
-        menuView.getMenu().add(1, R.id.search, 1, R.string.search);
+        menuView.getMenu().add(1, R.id.fridge, 1, R.string.fridge);
+        menuView.getMenu().add(1, R.id.cart, 1, R.string.cart);
         menuView.getMenu().add(1, R.id.cook, 1, R.string.cook);
         menuView.getMenu().add(1, R.id.favourite, 1, R.string.favourite);
         menuView.getMenu().add(1, R.id.user, 1, R.string.user);
 
-        menuView.getMenu().getItem(0).setIcon(R.drawable.add);
-        menuView.getMenu().getItem(1).setIcon(R.drawable.search);
+        menuView.getMenu().getItem(0).setIcon(R.drawable.fridge);
+        menuView.getMenu().getItem(1).setIcon(R.drawable.cart);
         menuView.getMenu().getItem(2).setIcon(R.drawable.cooker);
         menuView.getMenu().getItem(3).setIcon(R.drawable.heart);
         menuView.getMenu().getItem(4).setIcon(R.drawable.user);
