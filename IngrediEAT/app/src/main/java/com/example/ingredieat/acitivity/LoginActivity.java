569f25dade531f8a3762eeebaa7e1c4218677430
Juntao Wang <juntaow@bu.edu>
2020-04-12 22:49:41 -0400

    Improve logout

--- IngrediEAT/app/src/main/java/com/example/ingredieat/acitivity/LoginActivity.java
+++ IngrediEAT/app/src/main/java/com/example/ingredieat/acitivity/LoginActivity.java
@@ -60,7 +60,7 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-         super.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
